78380435f00dd8c85b1e72e0c1718ddaf0ce8ca1
Eduardo <eduardom4020@gmail.com>
2017-12-20 09:53:37 -0300

    Beggining to test Fused Location Provider!

--- app/src/main/java/compubiquia/ufc/br/sensortest/MainActivity.java
+++ app/src/main/java/compubiquia/ufc/br/sensortest/MainActivity.java
@@ -226,13 +226,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         filter.addAction(BluetoothDevice.ACTION_FOUND);
         registerReceiver(receiver, filter);
 
-        gpsHandler.postDelayed(new Runnable() {
-            public void run() {
-                sendGPSData();
-                //btAdapter.startDiscovery();
-                gpsHandler.postDelayed(this, HANDLER_DELAY);
-            }
-        }, HANDLER_DELAY);
+        sendGPSData();
     }
 
     protected void onResume() {
@@ -284,29 +278,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private void sendGPSData() {
 
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+//        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+//
+//            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                // TODO: Consider calling
+//                //    ActivityCompat#requestPermissions
+//                // here to request the missing permissions, and then overriding
+//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                //                                          int[] grantResults)
+//                // to handle the case where the user grants the permission. See the documentation
+//                // for ActivityCompat#requestPermissions for more details.
+//                return;
+//            }
+////            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+////                    GPS_TIME_INTERVAL, GPS_DISTANCE, this);
+//            Log.i("GPS", "repeating!");
+//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0.2f, this);
+//            Location gpslocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//
+//            String location = gpslocation.getLatitude()+" "+gpslocation.getLongitude();
+//            position_text.setText(location);
+//
+//            //mSocket.emit("set_location", location);
+//        }
 
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-//                    GPS_TIME_INTERVAL, GPS_DISTANCE, this);
-            Log.i("GPS", "repeating!");
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, this);
-            Location gpslocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-            String location = gpslocation.getLatitude()+" "+gpslocation.getLongitude();
-            position_text.setText(location);
-
-            //mSocket.emit("set_location", location);
-        }
     }
 
     private Emitter.Listener hpMessage = new Emitter.Listener() {
